e7cc08fe096889b5004913e378c379bdba42c7cb
Andrey Igoshin <a.y.igoshin@tinkoff.ru>
2018-10-27 15:46:32 +0300

    reformat file

--- notebooks/a.igoshin_a.lepigina_v.klykov_Lab_2.py
+++ notebooks/a.igoshin_a.lepigina_v.klykov_Lab_2.py
@@ -1,39 +1,42 @@
 import sys
 from os import path, curdir, scandir
 
-def Common_nodes(l1,l2):
+
+def Common_nodes(l1, l2):
     intersection = []
     for i in l1:
         if i in l2:
-         intersection.append(i)
-    return(intersection)
+            intersection.append(i)
+    return (intersection)
 
-def clique_heur (G, nodes):
+
+def clique_heur(G, nodes):
     d = nodes[0]
-    spisok_vershin=[]
-    clique=[]
-    kandidates=nodes
+    spisok_vershin = []
+    clique = []
+    kandidates = nodes
     clique.append(kandidates[d])
     # print(kandidates)
-    spisok_vershin=list(G.neighbors(kandidates[d]))
+    spisok_vershin = list(G.neighbors(kandidates[d]))
     # print('vershina ', kandidates[d], list(G.neighbors(kandidates[d])))
     kandidates.pop(d)
-    #print (kandidates)
+    # print (kandidates)
 
-    while len(spisok_vershin)!=0:
+    while len(spisok_vershin) != 0:
         # print(spisok_vershin)
 
         for i in range(len(kandidates)):
-            if spisok_vershin.count(kandidates[i])!=0:
+            if spisok_vershin.count(kandidates[i]) != 0:
                 break
         # print('vershina ', kandidates[i], list(G.neighbors(kandidates[i])))
         # print(kandidates[i])
-        spisok_vershin=Common_nodes(spisok_vershin,list(G.neighbors(kandidates[i])))
+        spisok_vershin = Common_nodes(spisok_vershin, list(G.neighbors(kandidates[i])))
         # print(spisok_vershin)
         clique.append(kandidates[i])
         kandidates.pop(i)
     # print (clique)
     return clique
+
 
 if __name__ == '__main__':
     modules_dir = path.join(curdir, '..', 'modules')
